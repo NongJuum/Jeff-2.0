852223aeebcf1415564d7527c85344a9b5ddd363
NongJuum <canon.non.7@gmail.com>
2026-08-19 10:58:30 +0700

    fix(types): add ChangeEvent contextual typing for JSX input/select onChange handlers

--- types/declarations.d.ts
+++ types/declarations.d.ts
@@ -4,6 +4,18 @@ declare namespace React {
   export type ReactNode = any;
   export type ReactElement = any;
   export type FC<P = {}> = (props: P) => any;
+  export interface ChangeEvent<T = any> {
+    target: T & { value: string; [key: string]: any };
+    currentTarget: T & { value: string; [key: string]: any };
+    [key: string]: any;
+  }
+  export interface MouseEvent<T = any> {
+    target: T;
+    currentTarget: T;
+    preventDefault(): void;
+    stopPropagation(): void;
+    [key: string]: any;
+  }
   export function useState<T>(initialState: T | (() => T)): [T, (value: T | ((prev: T) => T)) => void];
   export function useEffect(effect: () => void | (() => void), deps?: readonly any[]): void;
   export function useMemo<T>(factory: () => T, deps: readonly any[] | undefined): T;
@@ -16,8 +28,40 @@ declare module "react" {
 }
 
 declare namespace JSX {
+  interface DOMAttributes<T = any> {
+    children?: React.ReactNode;
+    onChange?: (event: React.ChangeEvent<T>) => void;
+    onClick?: (event: React.MouseEvent<T>) => void;
+    onKeyDown?: (event: any) => void;
+    onKeyUp?: (event: any) => void;
+    onSubmit?: (event: any) => void;
+    [key: string]: any;
+  }
+
+  interface HTMLAttributes<T = any> extends DOMAttributes<T> {
+    className?: string;
+    style?: any;
+    id?: string;
+    name?: string;
+    value?: any;
+    placeholder?: string;
+    type?: string;
+    disabled?: boolean;
+    open?: boolean;
+    href?: string;
+    target?: string;
+    rel?: string;
+    src?: string;
+    alt?: string;
+    [key: string]: any;
+  }
+
   interface IntrinsicElements {
-    [elemName: string]: any;
+    input: HTMLAttributes<HTMLInputElement>;
+    select: HTMLAttributes<HTMLSelectElement>;
+    textarea: HTMLAttributes<HTMLTextAreaElement>;
+    button: HTMLAttributes<HTMLButtonElement>;
+    [elemName: string]: HTMLAttributes<any>;
   }
   interface Element extends React.ReactElement {}
 }
